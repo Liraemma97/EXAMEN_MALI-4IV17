7ce2d7c5f2178c76cf911ceb0a5ee0ef39adf891
Emmanuel Lira <82550403+Liraemma97@users.noreply.github.com>
2021-05-12 20:50:16 -0500

    Create Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    /* Martínez Lira Irak Emmanuel 4IV17
+       */
+
+    float primer, segundo, multiplica, division;
+    printf("\n\n Programa 3");
+    printf("\n\n Saca el promedio de valores reales");
+    printf("\n\n Dame el primer valor real");
+    scanf("%f", &primer);
+    printf("\n\n Dame el segundo valor real");
+    scanf("%f", &segundo);
+    multiplica = primer * segundo;
+    division = primer / segundo;
+    printf("\n El producto es %f", multiplica);
+    printf("\n El cociente es %f", division);
+    return 0;
